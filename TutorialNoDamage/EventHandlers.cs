ee4a2c0049de018c38917298aae51abc023b3200
TheNewRoot <126024362+TheNewR00t@users.noreply.github.com>
2024-06-01 20:03:33 +0100

    v1.1
    Update

---
+++ TutorialNoDamage/EventHandlers.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+
+namespace TutorialNoDamage
+{
+    internal class EventHandlers
+    {
+        private readonly plugin plugin;
+        public EventHandlers(plugin plugin) => this.plugin = plugin;
+        public void HurtingPlayer(HurtingEventArgs e)
+        {
+            if (e.Attacker == null) return;
+
+            if (e.Player.IsTutorial)
+            {
+
+                if (e.Attacker.IsScp)
+                {
+                    e.IsAllowed = false;
+                    e.Player.DisableAllEffects();
+                }
+            }
+        }
+    }
+}
---
+++ TutorialNoDamage/plugin.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Features;
+using Exiled.CustomRoles.Events;
+using PlayerHandlers = Exiled.Events.Handlers.Player;
+
+namespace TutorialNoDamage
+{
+    public class plugin : Plugin<Config>
+    {
+        private EventHandlers eventHandlers;
+
+        public override void OnEnabled()
+        {
+            eventHandlers = new EventHandlers(this);
+            PlayerHandlers.Hurting += eventHandlers.HurtingPlayer;
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            PlayerHandlers.Hurting -= eventHandlers.HurtingPlayer;
+            eventHandlers = null;
+            base.OnDisabled();
+        }
+    }
+}
